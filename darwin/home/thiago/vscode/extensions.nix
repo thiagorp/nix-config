acac900f80660419f274f4f3ef2fe6ee08ff46a2
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2022-02-21 17:10:35 -0500

    Github PR VSCode extension

--- darwin/home/thiago/vscode/extensions.nix
+++ darwin/home/thiago/vscode/extensions.nix
@@ -1,6 +1,14 @@
-[{
-  name = "vscode-dbt-power-user";
-  publisher = "innoverio";
-  version = "0.5.13";
-  sha256 = "vpRthR2gZIk7y/fw0TeLtH4ceaGy10gIJufC+lzGcVk=";
-}]
+[
+  {
+    name = "vscode-dbt-power-user";
+    publisher = "innoverio";
+    version = "0.5.13";
+    sha256 = "vpRthR2gZIk7y/fw0TeLtH4ceaGy10gIJufC+lzGcVk=";
+  }
+  {
+    name = "vscode-pull-request-github";
+    publisher = "github";
+    version = "0.36.2";
+    sha256 = "AO9C1J/MWx+aY3ka3IQ38mO7q9XiGbiKEJ1ekB/a+jc=";
+  }
+]
