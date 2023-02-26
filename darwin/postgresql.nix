d281b2853ca57a2358af7ed1d3f8b02947e5ba4a
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2023-02-26 16:55:14 -0500

    Fix postgres

--- darwin/postgresql.nix
+++ darwin/postgresql.nix
@@ -15,6 +15,12 @@ in {
     settings = {
       timezone = "UTC";
     };
+
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all all trust
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
+    '';
   };
 
   launchd.user.agents.postgresql.serviceConfig = {
