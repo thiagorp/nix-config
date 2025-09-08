b2e0d37f42254bd13d977f53420c692d7edeb5f1
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2025-09-08 14:56:10 -0700

    Update flakes

--- home/thiago/ssh.nix
+++ home/thiago/ssh.nix
@@ -1,9 +1,9 @@
 {...}: {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host *
-        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    '';
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      identityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+    };
   };
 }
