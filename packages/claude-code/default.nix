c0abc67c8abe833d5e5757e947292e888a168fe5
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2025-10-07 09:36:08 -0700

    Upgrade Claude

--- packages/claude-code/default.nix
+++ packages/claude-code/default.nix
@@ -7,11 +7,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "claude-code";
-  version = "2.0.8";
+  version = "2.0.9";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-/9gFcsBXdF2sw8HdI8/h3QUX0MEzrL6zH/IT+1SW7Yo=";
+    hash = "sha256-DKad+i1krmEm6xq+aA8pSQ0pQK7ykExCkCuSQZw++Qw=";
   };
 
   nativeBuildInputs = [makeWrapper];
