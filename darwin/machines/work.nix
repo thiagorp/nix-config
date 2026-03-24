519260217a5a8e588233066af42759735f4203fe
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2026-03-24 13:36:16 -0700

    Align work nix settings with bootstrap-mercury

--- darwin/machines/work.nix
+++ darwin/machines/work.nix
@@ -24,12 +24,20 @@
       trusted-users = ["root" "thiago" "mercury" "nixbld"];
       trusted-public-keys = [
         "cache.mercury.com:yhfFlgvqtv0cAxzflJ0aZW3mbulx4+5EOZm6k3oML+I="
+        "cache.hub.internal.mercury.com:yhfFlgvqtv0cAxzflJ0aZW3mbulx4+5EOZm6k3oML+I="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
       trusted-substituters = [
         "https://cache.mercury.com"
+        "https://cache.hub.internal.mercury.com"
         "https://hydra.iohk.io"
       ];
+      warn-dirty = false;
+      max-jobs = "auto";
+      min-free = 21474836480;
+      max-free = 75161927680;
+      tarball-ttl = 86400;
+      allow-import-from-derivation = true;
     };
   };
 }
